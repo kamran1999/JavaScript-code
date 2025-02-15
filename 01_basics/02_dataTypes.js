cd752b705b668c57c8b853dbdfc7eeabdc16ebf2
Kamran Khan <125123161+kamran1999@users.noreply.github.com>
2025-02-15 12:49:32 +0000

    datatypes done

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,42 @@
+"use strict" // treat all js code as newer version
+
+
+//alert (3+3) // We are using nodejs not browser
+
+
+console.log(3
+    +
+    3);  // code readability should we high
+
+    console.log("kamran");
+
+
+    let name = "kamran"
+    let age = 24
+    let isLoggedIn = false
+    let state;
+
+
+    console.log(name, age, isLoggedIn);
+
+
+   // number => 2 to power 53
+   // BigInt
+   //String => ""
+   //boolean => true/false
+   //null => standalon value
+   //undefined =>
+    // symbol => unique
+
+
+
+   // object
+
+   console.log(typeof null); // object
+   console.log(typeof undefined); // undefined
+   
+   
+    
+    
+    
+    
