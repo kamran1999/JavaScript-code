c10f7cd6686db579300f12614e6bd6e646e6e864
Kamran Khan <125123161+kamran1999@users.noreply.github.com>
2025-02-15 09:52:09 +0000

    let const var  done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,29 @@
+const accountId = 277303
+let accountEmail = "kamran@google.com"
+var accountPassword = "0786"
+accountCity = "delhi"
+let accountState;
+
+
+//accountId = 303277  // Not allowed
+
+
+
+accountEmail= "lucky@yahoo.com"
+accountPassword= "6870"
+accountCity= "noida"
+
+
+console.log(accountId);
+
+
+
+/*prefer not to use var
+because of issue in block scoop and functional scoop */
+
+
+
+
+console.table([ accountId, accountEmail, accountPassword, accountCity, accountState]);
+
+
