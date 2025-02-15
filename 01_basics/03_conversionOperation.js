fa1710a3028979a39007d378ba38dd94eebe651f
Kamran Khan <125123161+kamran1999@users.noreply.github.com>
2025-02-15 14:44:32 +0000

    operations done

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -17,7 +17,7 @@ let valueInNumber = Number (score)
 let IsLoggedIn="kamran"
 
 let booleanIsLoggedIn = Boolean (IsLoggedIn)
-console.log(booleanIsLoggedIn);
+//console.log(booleanIsLoggedIn);
 
 
 // 1 => true;  0 => false
@@ -28,8 +28,72 @@ console.log(booleanIsLoggedIn);
 
 let someNumber = 24
 let stringNUmber = String (someNumber)
-console.log(stringNUmber);
-console.log(typeof stringNUmber);
+//console.log(stringNUmber);
+//console.log(typeof stringNUmber);
+
+
+
+
+//**************************************** Operations ********************************
+
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+
+
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2**2);
+//console.log(2/2);
+//console.log(2%2);
+
+
+let str1 = "kamran"
+let str2 = " khan"
+let str3 = str1 + str2
+//console.log(str3);
+
+
+//console.log("1" + 2);
+//console.log(1 + "2");
+//console.log("1" + 2 + 3);
+//console.log(1 + 2 + "3");
+
+
+//console.log(2 + 4 * 5 % 6); // without () parenthesis
+
+//console.log((2 + 4) *5 % 6); // with () parenthesis
+
+
+console.log(true);
+console.log(+true);
+console.log(+"");
+
+
+let num1, num2, num3
+
+num1=num2=num3= 2 + 2
+
+
+let gameCounter= 100
+//gameCounter++;
+++gameCounter
+console.log(gameCounter);
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
