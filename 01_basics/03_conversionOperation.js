6267c32ab250da35217d727fb3803dc0e0e051aa
Kamran Khan <125123161+kamran1999@users.noreply.github.com>
2025-02-15 13:24:16 +0000

    conversion of datatype done

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,38 @@
+let score= "kamran"
+
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber = Number (score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+// "33"=> 33
+// "33abcd" => NaN
+// true => 1;  false=> 0
+
+
+let IsLoggedIn="kamran"
+
+let booleanIsLoggedIn = Boolean (IsLoggedIn)
+console.log(booleanIsLoggedIn);
+
+
+// 1 => true;  0 => false
+// "" => false
+// "kamran" => true
+
+
+
+let someNumber = 24
+let stringNUmber = String (someNumber)
+console.log(stringNUmber);
+console.log(typeof stringNUmber);
+
+
+
+
+
+
